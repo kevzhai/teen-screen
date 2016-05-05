a14748bd17df7a72b8060327a4d1b4f50dea3321
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-05-05 02:24:11 -0700

    admin correctly collects all form data, redirects to /survey, discuss with team how to store/use form data (sessions vs database)

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,27 @@ router.get('/', function(req, res, next) {
       res.render('admin', { title: 'Teen Screen Admin Page', lines : content.toString().split('\n') });
   });
 
-router.post('/', function(req, res){
-  console.log(req.body);
-});
+	// http://stackoverflow.com/questions/19035373/how-do-i-redirect-in-expressjs-while-passing-some-context
+	function sendToSurvey(req, res) {
+	  // Prepare the context
+	  var context = req.dataProcessed;
+	  res.redirect('survey'); // need to send through session?
+	}
+
+	function processForm(req, res, next) {
+		console.log(req.body); // debugging purposes
+	  req.dataProcessed = req.body; // to-do: store / send data from form variables into session
+	  return next();
+	}
+
+	router.post('/', processForm, sendToSurvey);
+
+
+// router.post('/', function(req, res){
+//   console.log(req.body); // to-do: store / send data from form variables
+//   console.log("redirect");
+//   res.redirect(200, "./survey/initiate");
+// });
 
   
 });
