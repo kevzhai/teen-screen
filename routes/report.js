cf21ebf0c636a9797bdd6eafd9c0b7c4acbc119d
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-05-13 19:25:42 -0700

    uninstalled and reinstalled request module

--- routes/report.js
+++ routes/report.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET initiate survey page. */
+/* GET report page. */
 router.get('/', function(req, res, next) {
 	res.render('report', {
     title: 'Teen Screen Reports Page',
