399266980ef33a5a1167779394d47a6548adebcd
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-07-06 15:36:45 -0700

    offset in init.jade to match survey-question.json's index for sections

--- routes/init.js
+++ routes/init.js
@@ -20,6 +20,7 @@ router.get('/', function(req, res, next) {
 // http://stackoverflow.com/questions/19035373/how-do-i-redirect-in-expressjs-while-passing-some-context
 function processForm(req, res, next) {
   req.session.surveyParams = req.body;
+  console.log(req.session.surveyParams);
   res.status(302).redirect('/survey');
 }
 
