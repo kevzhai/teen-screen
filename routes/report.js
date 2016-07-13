6ecf3d2ba2876809a77989b12409688c4bd5971b
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-07-12 17:04:42 -0700

    got data to from server to reports.jade correctly

--- routes/report.js
+++ routes/report.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Screen = require('../lib/screen');
 
 /* GET report page. */
-router.get('/', function(req, res, next) {
-	res.render('report', {
-    title: 'Teen Screen Reports Page',
-    user: req.user ? req.user.fullName : '',
-    nav: true
+router.get('/', function(req, res) {
+  if (!req.user) {
+    return new Error("Must be logged in");
+  }
+  Screen.find({ admin: req.user.username }, function(err, reports) { // callback follows the pattern callback(error, results) http://mongoosejs.com/docs/queries.html
+    console.log(reports);
+    res.render('report', {
+      title: 'Teen Screen Reports Page',
+      reports: JSON.stringify(reports),
+      user: req.user.fullName,
+      nav: true
+    });
   });
 });
 
-router.get('/:subjectID', function(req, res, next) {
-  res.render('admin', {
-    title: 'Teen Screen Admin Page',
+router.get('/:subjectID', function(req, res) {
+  res.render('indiv_report', {
+    title: 'Generated Report',
+    subjectID: req.params.subjectID,
     user: req.user ? req.user.fullName : '',
     nav: true
   });
