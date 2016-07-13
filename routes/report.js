895cbcda2e7abc9ed309d3c76e2d0f5c1a0aea75
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-07-12 18:21:34 -0700

    basic generated report for a subject, need to figure out how to iterate over the nested array of responses now

--- routes/report.js
+++ routes/report.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 var Screen = require('../lib/screen');
 
-/* GET report page. */
-router.get('/', function(req, res) {
-  if (!req.user) {
+// TODO also check in Admin group
+function checkLoggedIn(user) { 
+  if (!user) {
     return new Error("Must be logged in");
   }
+}
+
+/* GET report page. */
+router.get('/', function(req, res) {
+  checkLoggedIn(req.user);
+  // only return reports administered by admin
   Screen.find({ admin: req.user.username }, function(err, reports) { // callback follows the pattern callback(error, results) http://mongoosejs.com/docs/queries.html
     console.log(reports);
     res.render('report', {
@@ -19,11 +25,17 @@ router.get('/', function(req, res) {
 });
 
 router.get('/:subjectID', function(req, res) {
-  res.render('indiv_report', {
-    title: 'Generated Report',
-    subjectID: req.params.subjectID,
-    user: req.user ? req.user.fullName : '',
-    nav: true
+  checkLoggedIn(req.user);
+
+  Screen.findOne({ subjectID: req.params.subjectID }, function(err, report) { // callback follows the pattern callback(error, results) http://mongoosejs.com/docs/queries.html
+    console.log('lone');
+    console.log(report);
+    res.render('indiv_report', {
+      title: `Report for ${req.params.subjectID}`,
+      report: JSON.stringify(report),
+      user: req.user.fullName,
+      nav: true
+    });
   });
 });
 
