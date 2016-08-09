29eb44558987a8ccdebc857fa104ad4926e51d1d
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-08-08 22:50:15 -0700

    symptom scale as sortable table, problem is that table header still shows even if there aren't any responses

--- routes/report.js
+++ routes/report.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var Screen = require('../lib/screen');
+var fs = require("fs");
+
+// http://stackoverflow.com/questions/12703098/how-to-get-a-json-file-in-express-js-and-display-in-view
+function readJsonFileSync(filepath, encoding){
+  if (typeof (encoding) == 'undefined'){
+    encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
 
 // TODO also check in Admin group
 function checkLoggedIn(user) { 
@@ -29,12 +39,15 @@ router.get('/:subjectID', function(req, res) {
 
   var subjectID = req.params.subjectID;
 
+  var symptomScale = readJsonFileSync('./private/symptom-scale.json');
+
   Screen.findOne({ subjectID: subjectID }, function(err, report) { // callback follows the pattern callback(error, results) http://mongoosejs.com/docs/queries.html
     // console.log('lone'); // debuq
     // console.log(report); // debuq
     res.render('indiv_report', {
       title: `Report for ${subjectID}`,
       report: JSON.stringify(report),
+      symptomScale: symptomScale,
       user: req.user.fullName,
       nav: true
     });
