05c2d9739dad4fb52be1ed1eb9fe859a514a6845
Kevin Zhai <kevzhai@users.noreply.github.com>
2016-08-21 16:06:30 -0700

    if logged in, homepage now correctly shows that you are logged in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var stormpath = require('express-stormpath');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.user);
+// GET home page.
+// https://docs.stormpath.com/nodejs/express/latest/user_data.html (use stormpath.getUser)
+router.get('/', stormpath.getUser, function(req, res) {
+  console.log('user', req.user);
   res.render('index', {
     title: 'Teen Screen',
     user: req.user ? req.user.fullName : '',
